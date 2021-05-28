cef8402a87ea173df218be8ebf7b2d89a256cefa
hammer-cy <h4mmer7ime@gmail.com>
2021-05-29 00:11:32 +0100

    idk

--- config.h
+++ config.h
@@ -6,7 +6,7 @@ static int centered = 1;                    /* -c option; centers dmenu on scree
 static int min_width = 700;                    /* minimum width when centered */
 /* -fn option overrides fonts[0]; default X11 font or font set */
 static const char *fonts[] = {
-	"Terminus:size=10",
+	"Dina:size=10",
 	"Siji",
 	"Noto Color Emoji:pixelsize=12:antialias=true:autohint=true"
 };
--- dmenu.c
+++ dmenu.c
@@ -958,7 +958,6 @@ setup(void)
 	swa.colormap = cmap;
 	swa.event_mask = ExposureMask | KeyPressMask | VisibilityChangeMask |
 		ButtonPressMask;
-//win = XCreateWindow(dpy, parentwin, x, y, mw, mh, 0,
 	win = XCreateWindow(dpy, parentwin, x, y, mw, mh, border_width,
 	                    depth, InputOutput, visual,
 	                    CWOverrideRedirect | CWBackPixel | CWColormap |  CWEventMask | CWBorderPixel, &swa);
