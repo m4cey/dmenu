f65c6d97dee4c9a5158a33f08b4c6405b476ff7e
hammer-cy <h4mmer7ime@gmail.com>
2021-05-29 00:16:21 +0100

    idk

--- config.h
+++ config.h
@@ -2,6 +2,7 @@
 /* Default settings; can be overriden by command line. */
 
 static int topbar = 1;                      /* -b  option; if 0, dmenu appears at bottom     */
+static int fuzzy = 1;                      /* -F  option; if 0, dmenu doesn't use fuzzy matching     */
 static int centered = 1;                    /* -c option; centers dmenu on screen */
 static int min_width = 700;                    /* minimum width when centered */
 /* -fn option overrides fonts[0]; default X11 font or font set */
